3aa021656fa050c1b11ad951f05e3aab9dafe2ca
Mithras <mithras.ftw@gmail.com>
2025-02-08 02:36:52 +0000

    sun app refactoring

--- appdaemon/apps/sun/sun.py
+++ appdaemon/apps/sun/sun.py
@@ -4,14 +4,20 @@ import globals
 class Sun(globals.Hass):
     async def initialize(self):
         config = self.args["config"]
-        self.light = config["light"]
+        self._sunset = config["sunset"]
+        self._sunrise = config["sunrise"]
 
         await self.run_at_sunset(self._sunset_callback_async)
         await self.run_at_sunrise(self._sunrise_callback_async)
 
     async def _sunset_callback_async(self, kwargs):
-        if await self.get_state(self.light) == "off":
-            await self.common.light_turn_nightlight_async(self.light)
+        # self.log(f"_sunset_callback_async: {self._sunset}")
+        for x in self._sunset:
+            await self.call_service(x["service"],
+                                    **x["data"])
 
     async def _sunrise_callback_async(self, kwargs):
-        await self.common.turn_off_async(self.light)
+        # self.log(f"_sunrise_callback_async: {self._sunrise}")
+        for x in self._sunrise:
+            await self.call_service(x["service"],
+                                    **x["data"])
