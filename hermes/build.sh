628fbe489f7ff86e553ca61245a278361f5a1676
Mithras <mithras.ftw@gmail.com>
2026-06-21 21:18:37 +0000

    hermes2

--- hermes/build.sh
+++ hermes/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-DOCKER_BUILDKIT=1 docker build . --pull --tag hermes-agent --tag hermes-agent:$(date +%Y%m%d)
+DOCKER_BUILDKIT=1 docker build . --tag hermes-agent --tag hermes-agent:$(date +%Y%m%d) # --pull
